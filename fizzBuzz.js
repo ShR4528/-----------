ce3597f44a07e09fac2de3c6fe1580f49559003c
Shr4528 <Shr4528@gmail.com>
2022-12-14 20:01:55 +0200

    commit

---
+++ fizzBuzz.js
@@ -0,0 +1,33 @@
+// const fizzBuzz = num => {
+//     for (let i = 1; i <= num; i++) {
+
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizzbuzz')
+//         }
+//         else if (i % 3 === 0) {
+//             console.log('fizz')
+//         }
+//         else if (i % 5 === 0) {
+//             console.log('buzz')
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+// fizzBuzz(15);
+
+
+//=====================================
+
+
+// const fibonacci = num => {
+// const result = [0, 1]
+//.      for (let i = 2; i <= num; i++) {
+    //         const prevNum1 = result[i - 1]
+    //         const prevNum2 = result[i - 2]
+    //         result.push(prevNum1 + prevNum2)
+
+//     }
+//     return result[num]
+// }
+// console.log(fibonacci(44))
